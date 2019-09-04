185495f57447a9580809fb890784953de5c958a2
Erica Mason <35743946+em006@users.noreply.github.com>
2019-09-03 23:28:35 -0500

    Config.js
    updated config file, rather than the original MM

---
+++ config.js
@@ -0,0 +1,112 @@
+/* Magic Mirror Config Sample
+ *
+ * By Michael Teeuw http://michaelteeuw.nl
+ * MIT Licensed.
+ *
+ * For more information how you can configurate this file
+ * See https://github.com/MichMich/MagicMirror#configuration
+ *
+ */
+
+var config = {
+	address: "localhost", // Address to listen on, can be:
+	                      // - "localhost", "127.0.0.1", "::1" to listen on loopback interface
+	                      // - another specific IPv4/6 to listen on a specific interface
+	                      // - "", "0.0.0.0", "::" to listen on any interface
+	                      // Default, when address config is left out, is "localhost"
+	port: 8080,
+	ipWhitelist: ["127.0.0.1", 
+				  "::ffff:127.0.0.1",
+				  "::1",
+				  "::ffff:130.184.7.109",
+				  "::ffff:10.35.60.64",
+				  "10.35.60.64",
+				  "10.35.95.216"], // Set [] to allow all IP addresses
+									// or add a specific IPv4 of 192.168.1.5 :
+									// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
+									// or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
+									// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
+
+	language: "en",
+	timeFormat: 12,
+	units: "imperial",
+
+	modules: [
+		{
+			module: 'MMM-TestModules/MMM-RemoteApp',
+			position: 'top_left',
+			updateTime: 1000
+		},
+		{
+			module: 'MMRemoteAPI',
+			position: 'top_left',
+			// username: "iruokgqc",
+// 			password: "iJrh6uT-j_uN"
+		}, 
+		{
+			module: "alert",
+		},
+		{
+			module: "updatenotification",
+			position: "top_bar"
+		},
+		{
+			module: "clock",
+			position: "top_left"
+		},
+		{
+			module: "calendar",
+			header: "US Holidays",
+			position: "top_left",
+			config: {
+				calendars: [
+					{
+						symbol: "calendar-check",
+						url: "webcal://www.calendarlabs.com/templates/ical/US-Holidays.ics"
+					}
+				]
+			}
+		},
+		{
+			module: "compliments",
+			position: "lower_third"
+		},
+		{
+			module: "currentweather",
+			position: "top_right",
+			config: {
+				location: "New York",
+				locationID: "4110486",  //ID from http://bulk.openweathermap.org/sample/; unzip the gz file and find your city
+				appid: "3794cb5474c16e7ff89b308aa561438c"
+			}
+		},
+		{
+			module: "weatherforecast",
+			position: "top_right",
+			header: "Weather Forecast",
+			config: {
+				location: "New York",
+				locationID: "4110486",  //ID from https://openweathermap.org/city
+				appid: "3794cb5474c16e7ff89b308aa561438c"
+			}
+		},
+		{
+			module: "newsfeed",
+			position: "bottom_bar",
+			config: {
+				feeds: [
+					{
+						title: "New York Times",
+						url: "http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml"
+					}
+				],
+				showSourceTitle: true,
+				showPublishDate: true
+			}
+		},
+	]
+
+};
+
+/*************** DO NOT EDIT THE LINE BELOW ***************/
+if (typeof module !== "undefined") {module.exports = config;}
